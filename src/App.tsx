4230cc93edc90dfac76d8a2349e93b79d264ad3e
jacob-myers <jakemyers190@gmail.com>
2025-04-02 19:42:31 -0400

    Making some adjustments to the navbar. Adding the help page and creating the routing system.

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {
 } from "react-router-dom";
 
 import MyNavbar from './components/Navbar'
+import HelpPage from './components/Help/HelpPage';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,26 +20,31 @@ function App() {
       <div className='App'>
         <MyNavbar />
       </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={
+          <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+          <h1>Vite + React</h1>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+            </button>
+            <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+          </div>
+        } />
+        <Route path="/help" element={<HelpPage/>} />
+      </Routes>
     </Router>
 
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 
 import { FaHandHoldingHeart, FaDog, FaHome } from "react-icons/fa";
+
+import logo from "/src/assets/logo_small.png";
 
 function MyNavbar() {
     const [expand, updateExpanded] = useState(false);
@@ -28,39 +29,44 @@ function MyNavbar() {
         expand="md"
         className={navColor ? "sticky" : "navbar"}
         >
-            <Container>
-                <Navbar.Toggle
-                    aria-controls="responsive-navbar-nav"
-                    onClick={() => {
-                        updateExpanded(!expand);
-                    }}
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ms-auto" defaultActiveKey="#home">
-                        <Nav.Item>
-                            <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                                <FaHome size={20} style={{ verticalAlign: "bottom", marginBottom: "3px" }} /> Home
-                            </Nav.Link>
-                        </Nav.Item>
+            <Navbar.Brand href="/" className="d-flex">
+                <img src={logo} className="img-logo" alt="brand" />
+            </Navbar.Brand>
+            <h1 className='nav-title'>
+                HART
+            </h1>
+            
+            <Navbar.Toggle
+                aria-controls="responsive-navbar-nav"
+                onClick={() => {
+                    updateExpanded(!expand);
+                }}
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ms-auto" defaultActiveKey="#home">
+                    <Nav.Item>
+                        <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+                            <FaHome size={20} style={{ verticalAlign: "bottom", marginBottom: "3px" }} /> Home
+                        </Nav.Link>
+                    </Nav.Item>
 
-                        <Nav.Item>
-                            <Nav.Link as={Link} to="/about" onClick={() => updateExpanded(false)}>
-                                <FaDog size={20} style={{ verticalAlign: "bottom", marginBottom: "3px" }} /> About
-                            </Nav.Link>
-                        </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link as={Link} to="/about" onClick={() => updateExpanded(false)}>
+                            <FaDog size={20} style={{ verticalAlign: "bottom", marginBottom: "3px" }} /> About
+                        </Nav.Link>
+                    </Nav.Item>
 
-                        <Nav.Item>
-                            <Nav.Link as={Link} to="/help" onClick={() => updateExpanded(false)}>
-                                <FaHandHoldingHeart size={20} style={{ verticalAlign: "bottom", marginBottom: "3px" }} /> How to Help
-                            </Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+                    <Nav.Item>
+                        <Nav.Link as={Link} to="/help" onClick={() => updateExpanded(false)}>
+                            <FaHandHoldingHeart size={20} style={{ verticalAlign: "bottom", marginBottom: "3px" }} /> How to Help
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     );
 }
