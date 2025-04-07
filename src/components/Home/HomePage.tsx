94b218b2128c18577316c638f4ee02cbbea3a84e
jacob-myers <jakemyers190@gmail.com>
2025-04-07 14:45:25 -0400

    Adding body for homepage.

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,15 +1,21 @@
 //import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import logoimg from '/src/assets/logo.png'
+import home_img from '/src/assets/home.jpg'
 
 const HomePage = () => {
 
     return (
         <div style={{ marginTop:"100px" }}>
-          <img src={logoimg} alt="Logo" className="logo" id='main-logo' />
-          <p className='content-text' style={{ marginTop:"20px" }}>
-            I don't really know what to put here
-          </p>
+          <img style={{ marginBottom:"20px" }} src={logoimg} alt="Logo" className="logo" id='main-logo' />
+          <div className='home-about-div'>
+            <img src={home_img} alt="Home" className='home-img' />
+            <p className='content-text home-about-text '>
+              Homeless Angels Rescue Team (HART) is a nonprofit organization in Texas rescuing homeless small dogs and helping them find forever homes in New England. <Link to="/about">Learn more</Link> about us and how to adopt, or <Link to='/help'>help us</Link> today in our mission to save as many homeless angels as possible by fostering in Texas or Rhode Island or donating critical funds.
+            </p>
+          </div>
+          
         </div>
     );
 };
